5fa2a53e48325ede5221f385deaa2ce9b55c05a2
eminah734 <wiewiorawojtek@gmail.com>
2025-07-01 18:21:58 +0200

    zmiany

--- Future_site/backend/firebase.js
+++ Future_site/backend/firebase.js
@@ -1,9 +1,10 @@
 // backend/firebase.js
-const { initializeApp, applicationDefault } = require('firebase-admin/app');
+const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
 const { getFirestore } = require('firebase-admin/firestore');
+const account = require('./react-native-dream-9213a-firebase-adminsdk-h9bc7-13c14882f8.json')
 
 const app = initializeApp({
-  credential: applicationDefault(),
+  credential: cert(account),
 });
 
 const db = getFirestore();
